2a5d6bc0ab3f5f07b74183ef692489d12ec880c0
poojakasar <poojakasar100@gmail.com>
2022-07-25 12:53:41 +0530

    files are added

---
+++ script.js
@@ -0,0 +1,72 @@
+const que_1 = document.getElementById('answer_1');
+const btn_1 = document.getElementById('question_1');
+btn_1.onclick = function(){
+   
+    if(que_1.style.display !=="none"){
+        que_1.style.display = "none";
+    }
+    else{
+        que_1.style.display = "block";
+    }
+}
+
+const que_2 = document.getElementById('answer_2');
+const btn_2 = document.getElementById('question_2');
+btn_2.onclick = function(){
+
+    if(que_2.style.display !== "none"){
+        que_2.style.display = "none";
+    }
+
+    else{
+        que_2.style.display = "block";
+    }
+}
+
+const que_3 = document.getElementById('answer_3');
+const btn_3 = document.getElementById('question_3');
+btn_3.onclick = function(){
+
+    if(que_3.style.display !== "none"){
+        que_3.style.display = " none";
+    }
+    else{
+        que_3.style.display = "block";
+    }
+}
+
+const que_4 = document.getElementById('answer_4');
+const btn_4 = document.getElementById('question_4');
+btn_4.onclick = function(){
+
+    if(que_4.style.display !== "none"){
+        que_4.style.display = "none";
+    }
+    else{
+        que_4.style.display = "block";
+    }
+}
+
+const que_5 = document.getElementById('answer_5');
+const btn_5 = document.getElementById('question_5');
+btn_5.onclick = function(){
+
+    if(que_5.style.display !== "none"){
+        que_5.style.display = "none";
+    }
+    else{
+        que_5.style.display = "block";
+    }
+}
+
+const que_6 = document.getElementById('answer_6');
+const btn_6 = document.getElementById('question_6');
+btn_6.onclick = function(){
+
+    if(que_6.style.display !== "none"){
+        que_6.style.display = "none";
+    }
+    else{
+        que_6.style.display = " block";
+    }
+}
